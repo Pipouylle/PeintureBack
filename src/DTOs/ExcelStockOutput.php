f96561cb2bfaf60c641204e45f562e96598e2f39
Timothé Natté <timothe.natte@etu.umontpllier.fr>
2025-04-02 16:41:46 +0200

    update: mise de l'of dans le stock

--- src/DTOs/ExcelStockOutput.php
+++ src/DTOs/ExcelStockOutput.php
@@ -10,12 +10,6 @@ class ExcelStockOutput
     public int $id;
 
     #[Groups(['excel:read'])]
-    public string $numeroAffaire;
-
-    #[Groups(['excel:read'])]
-    public string $nomAffaire;
-
-    #[Groups(['excel:read'])]
     public int $codeArticle;
 
     #[Groups(['excel:read'])]
@@ -25,5 +19,22 @@ class ExcelStockOutput
     public string $dateSortie;
 
     #[Groups(['excel:read'])]
+    public string $numeroAffaire;
+
+    #[Groups(['excel:read'])]
+    public string $nomAffaire;
+
+    #[Groups(['excel:read'])]
     public string $nomOperateur;
+
+    #[Groups(['excel:read'])]
+    public int $idOf;
+
+    #[Groups(['excel:read'])]
+    public string $nomSysteme;
+
+    #[Groups(['excel:read'])]
+    public string $typeSysteme;
+
+
 }
--- src/DataProvider/ExcelStockProvider.php
+++ src/DataProvider/ExcelStockProvider.php
@@ -25,12 +25,16 @@ class ExcelStockProvider implements ProviderInterface
         foreach ($stocks as $stock) {
             $dto = new ExcelStockOutput();
             $dto->id = $stock->getId();
+            $dto->dateEntree = $stock->getDateStockStock()->format('Y-m-d H:i:s');
             $dto->numeroAffaire = $stock->getOfStock() !== null ? $stock->getOfStock()->getIdDemandeOf()->getCommandeDemande()->getAffaireCommande()->getNomAffaire() : 'N/A';
             $dto->nomAffaire = $stock->getOfStock() !== null ? $stock->getOfStock()->getIdDemandeOf()->getCommandeDemande()->getAffaireCommande()->getNumeroAffaire() : 'N/A';
             $dto->codeArticle = $stock->getArticleStock()->getId();
             $dto->nomOperateur = $stock->getUserStock() !== null ? $stock->getUserStock()->getNameUser() : 'N/A';
-            $dto->dateEntree = $stock->getDateStockStock()->format('Y-m-d H:i:s');
+
             $dto->dateSortie = $stock->getDateSortieStock() !== null ? $stock->getDateSortieStock()->format('Y-m-d H:i:s') : 'N/A';
+            $dto->idOf = $stock->getOfStock() !== null ? $stock->getOfStock()->getId() : 'N/A';
+            $dto->nomSysteme = $stock->getOfStock() !== null ? $stock->getOfStock()->getIdDemandeOf()->getCommandeDemande()->getSystemeCommande()->getNomSysteme() : 'N/A';
+            $dto->typeSysteme = $stock->getOfStock() !== null ? $stock->getOfStock()->getIdDemandeOf()->getCommandeDemande()->getSystemeCommande()->getTypeSysteme() : 'N/A';
             // Add other fields as needed
 
             $dtos[] = $dto;
